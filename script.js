e4aeb0f402ba7fae71b104fd4f6d24755a4da436
coder-kirbs <135080557+coder-kirbs@users.noreply.github.com>
2023-07-14 11:17:37 -0500

    Rearrange variables for playRound

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 /* console.log("Hello, World!") */
 
-let playerSelection;
-let computerSelection;
 let rps = ["Rock", "Paper", "Scissors"];
-
 let getComputerChoice = Math.floor(Math.random() * rps.length)+1;
 console.log(getComputerChoice)
+
+let playerSelection= "rock";
+let computerSelection= getComputerChoice;
+
+
+function playRound(playerSelection, computerSelection) {
+
+}
