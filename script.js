c12ee30a9c881481cb861c89e3d0a32204586ff7
coder-kirbs <135080557+coder-kirbs@users.noreply.github.com>
2023-07-14 11:11:31 -0500

    Write computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello, World!")
+/* console.log("Hello, World!") */
+
+let playerSelection;
+let computerSelection;
+let rps = ["Rock", "Paper", "Scissors"];
+
+let getComputerChoice = Math.floor(Math.random() * rps.length)+1;
+console.log(getComputerChoice)
