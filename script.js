e07f985f756278d27eef7434561f1228df028759
coder-kirbs <135080557+coder-kirbs@users.noreply.github.com>
2023-07-14 11:24:17 -0500

    Set getComputerChoice function to convert number to string

--- script.js
+++ script.js
@@ -2,12 +2,14 @@
 
 let rps = ["Rock", "Paper", "Scissors"];
 let getComputerChoice = Math.floor(Math.random() * rps.length)+1;
-console.log(getComputerChoice)
+if (getComputerChoice===1) {
+    console.log("rock")
+} else if (getComputerChoice===2) {
+    console.log("paper")
+}
+  else {
+    console.log("scissors")
+}
 
 let playerSelection= "rock";
 let computerSelection= getComputerChoice;
-
-
-function playRound(playerSelection, computerSelection) {
-
-}
