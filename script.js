7094faf66320a1ddb5107e14f3c0d96d9db312dd
coder-kirbs <135080557+coder-kirbs@users.noreply.github.com>
2023-07-17 12:56:15 -0500

    Replicate RPS single round in console

--- script.js
+++ script.js
@@ -2,14 +2,24 @@
 
 let rps = ["Rock", "Paper", "Scissors"];
 let getComputerChoice = Math.floor(Math.random() * rps.length)+1;
-if (getComputerChoice===1) {
-    console.log("rock")
-} else if (getComputerChoice===2) {
-    console.log("paper")
-}
-  else {
-    console.log("scissors")
-}
-
 let playerSelection= "rock";
-let computerSelection= getComputerChoice;
+let computerSelection;
+
+    if (getComputerChoice===1) {
+        computerSelection="rock";
+        console.log("rock")
+    } else if (getComputerChoice===2) {
+        computerSelection="paper";
+        console.log("paper")
+    } else {
+        computerSelection="scissors";
+        console.log("scissors")
+    }
+
+    if (computerSelection=="rock" && playerSelection=="rock") {
+        console.log("Tie")
+    } else if (computerSelection=="paper" && playerSelection=="rock") {
+        console.log("You lose")
+    } else {
+        console.log("You win")
+    }
