9159c409efcc1aab02847cfe2c7151e2c4455865
coder-kirbs <135080557+coder-kirbs@users.noreply.github.com>
2023-07-20 17:12:17 -0500

    Adjust for case insensitivity

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 
 let rps = ["Rock", "Paper", "Scissors"];
 let getComputerChoice = Math.floor(Math.random() * rps.length)+1;
-let playerSelection= prompt("Rock, Paper, or Scissors?")
+let playerSelection= prompt("Rock, Paper, or Scissors?");
 let computerSelection;
+let playerScore=0;
+let computerScore=0;
 
     if (getComputerChoice===1) {
         computerSelection="rock";
@@ -16,25 +18,33 @@ let computerSelection;
         console.log("CPU chose scissors.")
     }
     
-    function playRound() {
-        if (computerSelection==="paper" && playerSelection==="rock") {
-            return ("You lose. Paper covers rock.");
-        } else if (computerSelection==="rock" && playerSelection==="scissors") {
-            return ("You lose. Rock breaks scissors.");
-        } else if (computerSelection==="scissors" && playerSelection==="paper") {
-            return ("You lose. Scissors cut paper.");
-        } else if (computerSelection==="paper" && playerSelection==="scissors") {
-            return ("You win! Scissors cut paper.");
-        } else if (computerSelection==="rock" && playerSelection==="paper") {
-            return ("You win! Paper covers rock.");
-        } else if (computerSelection==="scissors" && playerSelection==="rock") {
-            return ("You win! Rock breaks scissors.");
-        } else {
-            return ("Tie!")
-        }}
-    console.log(playRound())
 
 
+
+
+
+function playRound() {
+    if (computerSelection==="paper" && playerSelection.toLocaleLowerCase()==="rock") {
+        return ("You lose. Paper covers rock.");
+    } else if (computerSelection==="rock" && playerSelection.toLocaleLowerCase()==="scissors") {
+        return ("You lose. Rock breaks scissors.");
+    } else if (computerSelection==="scissors" && playerSelection.toLocaleLowerCase()==="paper") {
+        return ("You lose. Scissors cut paper.");
+    } else if (computerSelection==="paper" && playerSelection.toLocaleLowerCase()==="scissors") {
+        return ("You win! Scissors cut paper.");
+    } else if (computerSelection==="rock" && playerSelection.toLocaleLowerCase()==="paper") {
+        return ("You win! Paper covers rock.");
+    } else if (computerSelection==="scissors" && playerSelection.toLocaleLowerCase()==="rock") {
+        return ("You win! Rock breaks scissors.");
+    } else {
+        return ("Tie!")
+    }
+}
+console.log(playRound())
+
+function game() {
+
+}
 
 
 
