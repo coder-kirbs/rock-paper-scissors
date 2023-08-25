b60d581bb9c2f0eb60efde7bca8120f83f2a0950
Kirby <kirbyjallen@gmail.com>
2023-08-25 11:21:15 -0500

    Add HTML header text

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ let computerSelection;
 let playerScore=0;
 let computerScore=0;
 
-// console.log(game())
+//console.log(game())
 
 
 // functions below //
 
-/* function game() { 
+function game() { 
     console.log(playRound());
     console.log("You: "+playerScore);
     console.log("CPU: "+computerScore); 
@@ -28,7 +28,7 @@ let computerScore=0;
     console.log("You: "+playerScore);
     console.log("CPU: "+computerScore);
     console.log(checkWinner()) 
-} */
+}
 
 function playRound() {
     getComputerChoice = Math.floor(Math.random() * rps.length)+1;
@@ -38,29 +38,55 @@ function playRound() {
         computerSelection="paper";
     } else {
         computerSelection="scissors";
-    } let playerSelection= prompt("Rock, Paper, or Scissors?");
-    if (computerSelection==="paper" && playerSelection.toLocaleLowerCase()==="rock") {
-        ++computerScore;
-        return ("CPU chose paper. You lose. Paper covers rock.");
-    } else if (computerSelection==="rock" && playerSelection.toLocaleLowerCase()==="scissors") {
-        ++computerScore;
-        return ("CPU chose rock. You lose. Rock breaks scissors.");
-    } else if (computerSelection==="scissors" && playerSelection.toLocaleLowerCase()==="paper") {
-        ++computerScore;
-        return ("CPU chose scissors. You lose. Scissors cut paper.");
-    } else if (computerSelection==="paper" && playerSelection.toLocaleLowerCase()==="scissors") {
-        ++playerScore;
-        return ("CPU chose paper. You win! Scissors cut paper.");
-    } else if (computerSelection==="rock" && playerSelection.toLocaleLowerCase()==="paper") {
-        ++playerScore;
-        return ("CPU chose rock. You win! Paper covers rock.");
-    } else if (computerSelection==="scissors" && playerSelection.toLocaleLowerCase()==="rock") {
-        ++playerScore;
-        return ("CPU chose scissors. You win! Rock breaks scissors.");
-    } else {
-        return ("You and the CPU chose the same. Tie!")
-    }
+
+    } /*let playerSelection= alert("Rock, Paper, or Scissors?");
+        if (computerSelection==="paper" && playerSelection.toLocaleLowerCase()==="rock") {
+            ++computerScore;
+            return ("CPU chose paper. You lose. Paper covers rock.");
+        } else if (computerSelection==="rock" && playerSelection.toLocaleLowerCase()==="scissors") {
+            ++computerScore;
+            return ("CPU chose rock. You lose. Rock breaks scissors.");
+        } else if (computerSelection==="scissors" && playerSelection.toLocaleLowerCase()==="paper") {
+            ++computerScore;
+            return ("CPU chose scissors. You lose. Scissors cut paper.");
+        } else if (computerSelection==="paper" && playerSelection.toLocaleLowerCase()==="scissors") {
+            ++playerScore;
+            return ("CPU chose paper. You win! Scissors cut paper.");
+        } else if (computerSelection==="rock" && playerSelection.toLocaleLowerCase()==="paper") {
+            ++playerScore;
+            return ("CPU chose rock. You win! Paper covers rock.");
+        } else if (computerSelection==="scissors" && playerSelection.toLocaleLowerCase()==="rock") {
+            ++playerScore;
+            return ("CPU chose scissors. You win! Rock breaks scissors.");
+        } else {
+            return ("You and the CPU chose the same. Tie!")
+        }*/
+
+        let playerSelection= alert("Rock, Paper, or Scissors?");
+        if (computerSelection==="paper" && playerSelection.toLocaleLowerCase()==="rock") {
+            ++computerScore;
+            return ("CPU chose paper. You lose. Paper covers rock.");
+        } else if (computerSelection==="rock" && playerSelection.toLocaleLowerCase()==="scissors") {
+            ++computerScore;
+            return ("CPU chose rock. You lose. Rock breaks scissors.");
+        } else if (computerSelection==="scissors" && playerSelection.toLocaleLowerCase()==="paper") {
+            ++computerScore;
+            return ("CPU chose scissors. You lose. Scissors cut paper.");
+        } else if (computerSelection==="paper" && playerSelection.toLocaleLowerCase()==="scissors") {
+            ++playerScore;
+            return ("CPU chose paper. You win! Scissors cut paper.");
+        } else if (computerSelection==="rock" && playerSelection.toLocaleLowerCase()==="paper") {
+            ++playerScore;
+            return ("CPU chose rock. You win! Paper covers rock.");
+        } else if (computerSelection==="scissors" && playerSelection.toLocaleLowerCase()==="rock") {
+            ++playerScore;
+            return ("CPU chose scissors. You win! Rock breaks scissors.");
+        } else {
+            return ("You and the CPU chose the same. Tie!")
+        }
 }
+
+
 
 function checkWinner() {
     if (computerScore > playerScore) {
