8caee90486134abc38bffe5d7ccd9ec7db967dd2
Kirby <kirbyjallen@gmail.com>
2023-09-04 14:06:03 -0500

    Show real time score on page

--- script.js
+++ script.js
@@ -58,19 +58,6 @@ scissorsBtn.addEventListener('click', function(e) {
     playRound();
 });
 
-const resultsDiv = document.createElement('h5');
-const resultsHeading = document.querySelector('#resultsHeading')
-resultsHeading.append(resultsDiv);
-
-const yourScoreDOM = document.createElement('p');
-const yourScorePara = document.querySelector('#yourScorePara');
-yourScoreDOM.textContent = playerScore;
-yourScorePara.append(yourScoreDOM);
-
-const compScoreDOM = document.createElement('p');
-const compScorePara = document.querySelector('#compScorePara')
-compScoreDOM.textContent = computerScore
-compScorePara.append(compScoreDOM);
 
 
 function choices() {
@@ -88,21 +75,27 @@ function playRound() {
     if (computerSelection==="paper" && playerSelection==="rock") {
         ++computerScore;
         resultsDiv.textContent = "CPU chose paper. You lose. Paper covers rock.";
+        compScorePara.append(' '+computerScore);
     } else if (computerSelection==="rock" && playerSelection==="scissors") {
         ++computerScore;
         resultsDiv.textContent = "CPU chose rock. You lose. Rock breaks scissors.";
+        compScorePara.append(' '+computerScore);
     } else if (computerSelection==="scissors" && playerSelection==="paper") {
         ++computerScore;
         resultsDiv.textContent = "CPU chose scissors. You lose. Scissors cut paper.";
+        compScorePara.append(' '+computerScore);
     } else if (computerSelection==="paper" && playerSelection==="scissors") {
         ++playerScore;
         resultsDiv.textContent = "CPU chose paper. You win! Scissors cut paper.";
+        yourScorePara.append(' '+playerScore);
     } else if (computerSelection==="rock" && playerSelection==="paper") {
         ++playerScore;
         resultsDiv.textContent = "CPU chose rock. You win! Paper covers rock.";
+        yourScorePara.append(' '+playerScore);
     } else if (computerSelection==="scissors" && playerSelection==="rock") {
         ++playerScore;
         resultsDiv.textContent = "CPU chose scissors. You win! Rock breaks scissors.";
+        yourScorePara.append(' '+playerScore);
     } else {
         resultsDiv.textContent = "You and the CPU chose the same. Tie!"
     }
@@ -111,35 +104,29 @@ function playRound() {
     checkWinner();
 };
 
-/*let playerSelection= alert("Rock, Paper, or Scissors?");
-if (computerSelection==="paper" && playerSelection==="rock") {
-    ++computerScore;
-    return ("CPU chose paper. You lose. Paper covers rock.");
-} else if (computerSelection==="rock" && playerSelection==="scissors") {
-    ++computerScore;
-    return ("CPU chose rock. You lose. Rock breaks scissors.");
-} else if (computerSelection==="scissors" && playerSelection==="paper") {
-    ++computerScore;
-    return ("CPU chose scissors. You lose. Scissors cut paper.");
-} else if (computerSelection==="paper" && playerSelection==="scissors") {
-    ++playerScore;
-    return ("CPU chose paper. You win! Scissors cut paper.");
-} else if (computerSelection==="rock" && playerSelection==="paper") {
-    ++playerScore;
-    return ("CPU chose rock. You win! Paper covers rock.");
-} else if (computerSelection==="scissors" && playerSelection==="rock") {
-    ++playerScore;
-    return ("CPU chose scissors. You win! Rock breaks scissors.");
-} else {
-    return ("You and the CPU chose the same. Tie!")
-}*/
+const resultsDiv = document.createElement('h5');
+const resultsHeading = document.querySelector('#resultsHeading')
+const resultsWinner = document.createElement('h4')
+resultsHeading.append(resultsDiv);
+
+const yourScoreDOM = document.createElement('p');
+const yourScorePara = document.querySelector('#yourScorePara');
+yourScoreDOM.textContent = playerScore;
+yourScorePara.append(' '+playerScore);
+
+const compScoreDOM = document.createElement('p');
+const compScorePara = document.querySelector('#compScorePara')
+compScoreDOM.textContent = computerScore
+compScorePara.append(' '+computerScore);
 
 
 function checkWinner() {
     if (computerScore===5) {
-        console.log ("Game over! You lose!")
+        resultsWinner.textContent = "Game over! You lose!"
+        resultsDiv.appendChild(resultsWinner);
     } else if (playerScore===5) {
-        console.log ("Game over! You win!")
+        resultsWinner.textContent = "Game over! You win!"
+        resultsDiv.appendChild(resultsWinner);
     } else 
         return 
 }
